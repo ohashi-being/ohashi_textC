b3a16719ac446a62a27ee7cb546e9d403690b763
ohashi <ohashi@beingcorp.co.jp>
2025-10-27 12:03:39 +0900

    #581828 大橋_C#テキスト学習
    3章の章末問題の解答を作成しました。

---
+++ 章末問題3/Practice3-1/Program3-1.cs
@@ -0,0 +1,84 @@
+﻿/* 問題3-1
+ * 以下のリストが定義してあります。
+ * var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+ * このリストに対して、ラムダ式を使用して、次のコードを書いてください。
+ * 
+ * 1.List<T>のExistsメソッドを使用して、8か9で割り切れる数があるかどうかを調べ、
+ *   結果を表示してください。
+ *   
+ * 2.List<T>のForEachメソッドを使用して、各要素を2.0で割った値を表示してください。
+ * 
+ * 3.LINQのWhereメソッドを使用して、50以上の要素を列挙し、結果を表示してください。
+ * 
+ * 4.LINQのSelectメソッドを使用して、各要素を2倍し、その結果をList<int>に格納してください。
+ *   その後、List<int>の要素をすべて表示してください。*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice3_1 {
+    internal class Program {
+        static void Main(string[] args) {
+            var wNumbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+            Console.WriteLine("--------解答1-------");
+            CheckDivision(wNumbers, 8, 9);
+            Console.WriteLine("--------解答2-------");
+            DivideNumber(wNumbers, 2.0);
+            Console.WriteLine("--------解答3-------");
+            ShowOver(wNumbers, 50);
+            Console.WriteLine("--------解答4-------");
+            ShowList(wNumbers);
+        }
+
+        // 1の解答
+        /// <summary>
+        /// 入力した2つの数で割り切れる数があるかどうかを調べる
+        /// </summary>
+        /// <param name="vNumbers">数字のリスト</param>
+        /// <param name="vFirst">割れてほしい1つ目の数字</param>
+        /// <param name="vSecond">割れてほしい2つ目の数字</param>
+        static void CheckDivision(List<int> vNumbers, int vFirst, int vSecond) {
+            bool wExist = vNumbers.Exists(n => n % vFirst == 0 || n % vSecond == 0);
+            if (wExist)
+                Console.WriteLine($"{vFirst} か {vSecond}で割り切れる数がある ");
+            else
+                Console.WriteLine($"{vFirst} か {vSecond}で割り切れる数がない ");
+        }
+
+        // 2の解答
+        /// <summary>
+        /// 入力した数で各要素を割った値を表示する
+        /// </summary>
+        /// <param name="vNumbers">数字のリスト</param>
+        /// <param name="vDivisor">割りたい数</param>
+        static void DivideNumber(List<int> vNumbers, double vDivisor) {
+            vNumbers.ForEach(n => Console.WriteLine(n / vDivisor));
+        }
+
+        // 3の解答
+        /// <summary>
+        /// 入力した数以上の要素を列挙する
+        /// </summary>
+        /// <param name="vNumbers">数字のリスト</param>
+        /// <param name="vOver">基準となる数</param>
+        static void ShowOver(List<int> vNumbers, int vOver) {
+            var wOver = vNumbers.Where(n => n >= vOver);
+            foreach (var wNumber in wOver) {
+                Console.WriteLine(wNumber);
+            }
+        }
+
+        // 4の解答
+        /// <summary>
+        /// 値を2倍にして 表示する
+        /// </summary>
+        /// <param name="vNumbers">数字のリスト</param>
+        /// <returns></returns>
+        static List<int> DoubleNumber(List<int> vNumbers) => vNumbers.Select(n => n * 2).ToList();
+        static void ShowList(List<int> vNumbers) {
+            List<int> wNumbers = DoubleNumber(vNumbers);
+            wNumbers.ForEach(n => Console.WriteLine(n));
+        }
+    }
+}
---
+++ 章末問題3/Practice3-2/Program3-2.cs
@@ -0,0 +1,88 @@
+﻿/* 問題3-2
+ * 以下のリストが定義してあります。
+ * var names = new List<string> { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong"};
+
+ * このリストに対して、ラムダ式を使用して、次のコードを書いてください。
+ * 
+ * 1.コンソールから入力した都市名が何番目に格納されているかList<T>のFindIndexメソッドを使用して調べ、結果を表示してください。
+ *   見つからなかったら-1を表示してください。
+ *   また、コンソールからの入力にはConsole.ReadLineメソッドを使用してください。
+ *   
+ * 2.LINQのCountメソッドを使い、小文字の'o'が含まれる都市名の数を調べ、結果を表示してください。
+ * 
+ * 3.LINQのWhereメソッドを使用して、小文字の"o"が含まれる都市名を列挙し、結果を表示してください。
+ * 
+ * 4.LINQのWhereメソッドとSelectメソッドを使い、"B"で始まる都市名の文字数を列挙し、結果を表示してください。
+ *   都市名を表示する必要はありません*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice3_2 {
+    internal class Program {
+        static void Main(string[] args) {
+            var wNames = new List<string> { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong" };
+            Console.WriteLine("--------解答1-------");
+            FindCity(wNames);
+            Console.WriteLine("--------解答2-------");
+            CountContainingCharacter(wNames, "o");
+            Console.WriteLine("--------解答3-------");
+            ShowContainingCharacter(wNames, "o");
+            Console.WriteLine("--------解答4-------");
+            ShowStartingCharacter(wNames, "B");
+        }
+
+        // 1の解答
+        /// <summary>
+        /// コンソールに入力された都市名がリストの何番目に格納されているかを調べる
+        /// </summary>
+        /// <param name="vNames">都市名のリスト</param>
+        static void FindCity(List<string> vNames) {
+            Console.Write("都市名を入力してください:");
+            string wInput = Console.ReadLine();
+            int wIndex = vNames.FindIndex(name => name == wInput);
+            if (wIndex == -1)
+                Console.WriteLine("見つかりませんでした");
+            else
+                Console.WriteLine($"{wIndex} 番目に格納されています");
+        }
+
+        // 2の解答
+        /// <summary>
+        /// 入力した文字が含まれる都市名の数を調べる
+        /// </summary>
+        /// <param name="vNames">都市名のリスト</param>
+        /// <param name="vKomoji">調べたい文字</param>
+        static void CountContainingCharacter(List<string> vNames, string vKomoji) {
+            int wCount = vNames.Count(name => name.Contains(vKomoji));
+            Console.WriteLine($"{vKomoji} が含まれる都市名の数は {wCount} つ");
+        }
+
+        // 3の解答
+        /// <summary>
+        /// 入力した文字が含まれる都市名を列挙する
+        /// </summary>
+        /// <param name="vNames">都市名のリスト</param>
+        /// <param name="vKomoji">調べたい文字</param>
+        static void ShowContainingCharacter(List<string> vNames, string vKomoji) {
+            var wNames = vNames.Where(name => name.Contains(vKomoji));
+            foreach (var wName in wNames) {
+                Console.WriteLine(wName);
+            }
+        }
+
+        // 4の解答
+        /// <summary>
+        /// 入力した文字で始まる都市名の文字数を列挙する
+        /// </summary>
+        /// <param name="vNames">都市名のリスト</param>
+        /// <param name="vStart">一番目の文字</param>
+        static void ShowStartingCharacter(List<string> vNames, string vStart) {
+            var wLengths = vNames.Where(name => name.StartsWith(vStart))
+                                .Select(name => name.Length);
+            foreach (var wLength in wLengths) {
+                Console.WriteLine(wLength);
+            }
+        }
+    }
+}
