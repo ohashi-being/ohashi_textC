6f9133799f8df2cd60ffd13cdf72116df3a50253
ohashi <ohashi@beingcorp.co.jp>
2025-11-04 11:13:17 +0900

    #581828 大橋_C#テキスト学習
    4章の章末問題の解答を作成しました。

---
+++ 章末問題4/Practice4-1/YearMonth.cs
@@ -0,0 +1,69 @@
+﻿/* 問題4.1
+ * 
+ * 以下の順にYearMonthクラスを定義してください。
+ * 
+ * 1.年(Year)と月(Month)の2つのプロパティを持つクラスYearMonthを定義してください。
+ *   このとき、2つのプロパティは読み取り専用にし、値はコンストラクタで指定できるようにしてください。
+ *   引数で渡される月の値は1から2の範囲にあるものと仮定してかまいません。
+ * 
+ * 2.YearMonthクラスに、Check21Centuryプロパティを追加してください。
+ *   2001年から2100年までが21世紀です。この処理では加減乗除は行わないでください。
+ * 
+ * 3.YearMonthクラスに、1ヵ月後を求めるGetAfterOneMonthメソッドを追加してください。
+ *   このとき、自分自身のプロパティは変更せずに、新たなYearMonthオブジェクトを生成し、その値を返してください。
+ *   12月の時の処理に注意してください。
+ * 
+ * 4.ToStringメソッドをオーバーライドしてください。
+ *   結果は、"2017年8月"といった形式にしてください。 */
+
+namespace Practice4_1 {
+    public class YearMonth {
+        // 1の解答
+        /// <summary>
+        /// 年
+        /// </summary>
+        public int Year { get; }
+        /// <summary>
+        /// 月
+        /// </summary>
+        public int Month { get; }
+        /// <summary>
+        /// 年月を入力するコンストラクタ
+        /// </summary>
+        /// <param name="vYear">年</param>
+        /// <param name="vMonth">月</param>
+        public YearMonth(int vYear, int vMonth) {
+            this.Year = vYear;
+            this.Month = vMonth;
+        }
+        // 2の解答
+        /// <summary>
+        /// 年が21世紀かどうか判定する
+        /// </summary>
+        public bool Check21Century {
+            get {
+                return 2001 <= this.Year && this.Year <= 2100;
+            }
+        }
+        // 3の解答
+        /// <summary>
+        /// １ヵ月後の年月を取得する
+        /// </summary>
+        /// <returns>1ヵ月後の年と月</returns>
+        public YearMonth GetAfterOneMonth() {
+            if (this.Month == 12) {
+                return new YearMonth(this.Year + 1, 1);
+            } else {
+                return new YearMonth(this.Year, this.Month + 1);
+            }
+        }
+        // 4の解答
+        /// <summary>
+        /// ToStringメソッドをオーバーライドし、形式を変換する
+        /// </summary>
+        /// <returns>2017年8月という形式に変換</returns>
+        public override string ToString() {
+            return $"{this.Year} 年 {this.Month} 月";
+        }
+    }
+}
---
+++ 章末問題4/Practice4-2/Program4-2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using Practice4_1;
+
+/* 問題4.2
+* 
+* 問題4.1で定義したYearMonthクラスを使って、次のコードを書いてください。
+* 
+* 1.YearMonthを要素に持つ配列を定義し、初期値として5つのYearMonthオブジェクトをセットしてください。
+* 
+* 2.この配列の要素(YearMonthオブジェクト)をすべて列挙し、その値をコンソールに出力してください。
+* 
+* 3.配列の中の最初に見つかった21世紀のYearMonthオブジェクトを返すメソッドを書いてください。
+*   見つからなかった場合は、nullを返してください。
+*   foreach文を使って実装してください。
+* 
+* 4.3で作成したメソッドを呼び出し、最初に見つかった21世紀のデータの年を表示してください。
+*   見つからなければ、"21世紀のデータはありません"を表示してください。
+*   
+* 5.配列に格納されているすべてのYearMonthの1ヵ月後を求め、その結果を新たな配列にいれてください。
+*   その後、その配列の要素の内容(年月)を順に表示してください。
+*   LINQを使えるところはLINQを使って実装してみてください。*/
+
+namespace Practice4_2 {
+    public class Program {
+        static void Main(string[] args) {
+            // 1の解答
+            var wYearMonthArray = new YearMonth[] {
+                new YearMonth(1019,4),
+                new YearMonth(1011,8),
+                new YearMonth(1030,7),
+                new YearMonth(2101,1),
+                new YearMonth(2000,12),
+            };
+            // 2の解答
+            ShowArray(wYearMonthArray);
+            Console.WriteLine("--------------------------");
+            // 4の解答
+            Show21Century(wYearMonthArray);
+            Console.WriteLine("--------------------------");
+            // 5の解答
+            ShowArray(wYearMonthArray.Select(x => x.GetAfterOneMonth()).ToArray());
+            Console.WriteLine("--------------------------");
+            Show21Century(wYearMonthArray.Select(x => x.GetAfterOneMonth()).ToArray());
+        }
+        // 2の解答
+        /// <summary>
+        /// 配列の要素をコンソールに出力する
+        /// </summary>
+        /// <param name="vYearMonths">YearMonthオブジェクトを持つ配列</param>
+        static void ShowArray(YearMonth[] vYearMonths) {
+            foreach (var wYearMonth in vYearMonths) {
+                Console.WriteLine(wYearMonth);
+            }
+        }
+        // 3の解答
+        /// <summary>
+        ///  配列の中の最初に見つかった21世紀のYearMonthオブジェクトを返す
+        /// </summary>
+        /// <param name="vYearMonths">YearMonthオブジェクトを持つ配列</param>
+        /// <returns>最初に見つかった21世紀のYearMonthオブジェクト ※見つからなかった場合は、nullを返す</returns>
+        static YearMonth Search21Century(YearMonth[] vYearMonths) {
+            foreach (var wYearMonth in vYearMonths) {
+                if (wYearMonth.Check21Century) {
+                    return wYearMonth;
+                }
+            }
+            return null;
+        }
+        // 4の解答
+        /// <summary>
+        /// 最初に見つかった21世紀のデータの年を表示する
+        /// </summary>
+        /// <param name="vYearMonths">YearMonthオブジェクトを持つ配列</param>
+        static void Show21Century(YearMonth[] vYearMonths) {
+            var wFirst21Century = Search21Century(vYearMonths);
+            if (wFirst21Century != null) {
+                Console.WriteLine(wFirst21Century.Year);
+            } else { Console.WriteLine("21世紀のデータはありません"); }
+        }
+    }
+}
