7ffd78871c513a6dafe173edbdb420c195731d8a
ohashi <ohashi@beingcorp.co.jp>
2025-11-18 14:07:06 +0900

    #581840 [C#テキスト]8章演習問題
    8章の章末問題の解答を作成しました。

---
+++ 章末問題8/Practice8-2/Program8-2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+/* 問題8.2
+次の週の指定曜日を求めるメソッドを定義してください。
+
+*/
+
+namespace Practice8_2 {
+    internal class Program {
+        static void Main(string[] args) {
+            var wToday = DateTime.Today;
+            ShowNextWeekDay(wToday,"土");
+        }
+        /// <summary>
+        /// 基準日から見て、次の週の指定曜日の日付を表示する
+        /// </summary>
+        /// <param name="vBaseDate">基準となる日</param>
+        /// <param name="vDayOfWeekJp">求めたい曜日(漢字一字)</param>
+        static void ShowNextWeekDay(DateTime vBaseDate , string vDayOfWeekJp) {
+            var wJpWeek = new Dictionary<string, DayOfWeek>
+        {
+            {"日", DayOfWeek.Sunday},
+            {"月", DayOfWeek.Monday},
+            {"火", DayOfWeek.Tuesday},
+            {"水", DayOfWeek.Wednesday},
+            {"木", DayOfWeek.Thursday},
+            {"金", DayOfWeek.Friday},
+            {"土", DayOfWeek.Saturday}
+        };
+            if (!wJpWeek.TryGetValue(vDayOfWeekJp, out DayOfWeek vDayOfWeek)) {
+                Console.WriteLine("漢字一字で入力してください。（例：月,火,水）");
+                return;
+            }
+            var wDayDif = vDayOfWeek - vBaseDate.DayOfWeek;
+            var wNextweekDate = vBaseDate.AddDays(wDayDif + 7);
+            var wJapanInfo = new CultureInfo("ja-JP");
+            wJapanInfo.DateTimeFormat.Calendar = new JapaneseCalendar();
+            Console.WriteLine($"次の週の{wJapanInfo.DateTimeFormat.GetShortestDayName(vDayOfWeek)}曜日は{wNextweekDate.Month}月{wNextweekDate.Day}日");
+        }
+    }
+}
---
+++ 章末問題8/Practice8-3/TimeWatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice8_3 {
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TimeWatch {
+        /// <summary>
+        /// 計測開始時刻
+        /// </summary>
+        private DateTime FStartTime;
+        /// <summary>
+        /// 処理時間の計測を開始する
+        /// </summary>
+        public void Start() {
+            FStartTime = DateTime.Now;
+        }
+        /// <summary>
+        /// 開始からの経過時間を返す
+        /// </summary>
+        /// <returns>処理時間</returns>
+        public TimeSpan Stop() {
+            return DateTime.Now - FStartTime;
+        }
+    }
+}
