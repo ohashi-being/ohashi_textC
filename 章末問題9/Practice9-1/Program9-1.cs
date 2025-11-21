773f79fb9a4ba7856bb56a3a7b59e9feeac52223
ohashi <ohashi@beingcorp.co.jp>
2025-11-21 17:05:33 +0900

    #581839 [C#テキスト]7章演習問題
    問題9.1に解答しました

--- 章末問題9/Practice9-1/Program9-1.cs
+++ 章末問題9/Practice9-1/Program9-1.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 /* 問題9.1
 以下の問題を解いてください。
 
@@ -17,8 +16,50 @@ using System.Threading.Tasks;
 3.このプログラムを、File.ReadLinesメソッドを利用して書き換えてください。    
 */
 namespace Practice9_1 {
-    internal class Program {
-        static void Main(string[] args) {
+    class Program {
+        static void Main(string[] vArgs) {
+            string wExeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string wFilePath = Path.Combine(wExeDir, "Program9-1.cs");
+            if (!File.Exists(wFilePath)) {
+                throw new FileNotFoundException($"ファイルが存在しません: {wFilePath}");
+            }
+            CountClassWithStreamReader(wFilePath, " class ");
+            CountClassWithReadAllLines(wFilePath, " class ");
+            CountClassWithReadLines(wFilePath, " class ");
+        }
+        /// <summary>
+        ///StreamReaderクラスを使用し、ファイルを読み込み、指定した文字列が含まれる行数をカウントします。
+        /// </summary>
+        /// <param name="vFilePath">読み込み対象のファイルパス</param>
+        /// <param name="vString">検索対象の文字列</param>
+        private static void CountClassWithStreamReader(string vFilePath, string vString) {
+            var wStringCount = 0;
+            using (var wStreamReader = new StreamReader(vFilePath)) {
+                while (!wStreamReader.EndOfStream) {
+                    var wLine = wStreamReader.ReadLine();
+                    if (wLine.Contains($"{vString}"))
+                        wStringCount++;
+                }
+            }
+            Console.WriteLine($"{vString}は{wStringCount}つあります");
+        }
+        /// <summary>
+        /// File.ReadAllLinesメソッドを使用し、ファイルを読み込み、指定した文字列が含まれる行数をカウントします。
+        /// </summary>
+        /// <param name="vFilePath">読み込み対象のファイルパス</param>
+        /// <param name="vString">検索対象の文字列</param>
+        private static void CountClassWithReadAllLines(string vFilePath, string vString) {
+            var wStringCount = File.ReadAllLines(vFilePath).Count(x => x.Contains($"{vString}"));
+            Console.WriteLine($"{vString}は{wStringCount}つあります");
+        }
+        /// <summary>
+        /// File.ReadLinesメソッドを使用し、ファイルを読み込み、指定した文字列が含まれる行数をカウントします。
+        /// </summary>
+        /// <param name="vFilePath">読み込み対象のファイルパス</param>
+        /// <param name="vString">検索対象の文字列</param>
+        private static void CountClassWithReadLines(string vFilePath, string vString) {
+            var wStringCount = File.ReadLines(vFilePath).Count(x => x.Contains($"{vString}"));
+            Console.WriteLine($"{vString}は{wStringCount}つあります");
         }
     }
 }
