e535d47deba7a23d9d950eb8e0d1cfc05c786210
ohashi <ohashi@beingcorp.co.jp>
2025-12-02 14:24:08 +0900

    #581842[C#テキスト]10章演習問題
    10章の章末問題を解答しました

--- 章末問題10/Practice10-5/Program10-5.cs
+++ 章末問題10/Practice10-5/Program10-5.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 /* 問題10.5
 HTMLファイルを読み込み、<DIV>や<P>などのタグ名が大文字になっているものを小文字のタグに変換してください。
-火の運荒場、<DIV class="myBoxid="myId">のように属性が記述されている場合にも対応してください。
+可能ならば、<DIV class="myBoxid="myId">のように属性が記述されている場合にも対応してください。
 属性の中には'<'や'>'が含まれないものとします。
 */
 namespace Practice10_5 {
@@ -17,18 +14,25 @@ namespace Practice10_5 {
             string wFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "sample.html");
             if (!File.Exists(wFilePath)) throw new FileNotFoundException($"ファイルが存在しません: {wFilePath}");
             var wLines = File.ReadAllLines(wFilePath);
-            File.WriteAllLines(wFilePath, wLines.Select(x => ConvertTagToLower(x)).ToArray());
-            foreach (var wLine in wLines)
-                Console.WriteLine(File.ReadAllLines(wFilePath));
+            var wConvertedLines = wLines.Select(x => ConvertTagToLower(x)).ToArray();
+            File.WriteAllLines(wFilePath, wConvertedLines);
+            foreach (var wLine in wConvertedLines)
+                Console.WriteLine(wLine);
             Console.WriteLine($"{Environment.NewLine}終了するには何かキーを押してください...");
             Console.ReadKey();
         }
+        /// <summary>
+        /// 受け取った文字列内の大文字タグを小文字タグに変換する
+        /// </summary>
+        /// <param name="vLine">HTMLファイル内の文字列</param>
+        /// <returns>小文字タグに変換された文字列</returns>
         static string ConvertTagToLower(string vLine) {
-            var wMatches = Regex.Matches(vLine, @"<\s*(/?)([A-Z]+)(\s[^<>]*)?>");
-            if (wMatches.Count == 1) {
-            } else if (wMatches.Count == 0) {
-            } else {
-            //}return $"<{x.Groups[1].Value}{x.Groups[2].Value.ToLower()}{x.Groups[3].Value}>";
+            return Regex.Replace(vLine, @"<\s*(/?)([A-Z]+)(\s[^<>]*)?>", x => {
+                var wPrefix = x.Groups[1].Value;
+                var wTagName = x.Groups[2].Value.ToLower();
+                var wAttributes = x.Groups[3].Value;
+                return $"<{wPrefix}{wTagName}{wAttributes}>";
+            });
         }
     }
 }
--- 章末問題10/Practice10-6/Program10-6.cs
+++ 章末問題10/Practice10-6/Program10-6.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 /* 問題10.6
 5文字の回文とマッチする正規表現を作成してください。
@@ -13,17 +10,24 @@ namespace Practice10_6 {
         static void Main(string[] args) {
             var wStrings = new List<string> {
                 "level",
-                "deified",
-                "12321",
+                "racecar",
+                "12321",  
+                "ab@ba",  
+                "ohashi", 
+                " ",      
+                "",
+                "しかるかし",
+                "たけやぶやけた",
+                "シカルカシ",
+                "タケヤブヤケタ",
+                "大橋陸",
+                "大橋陸橋大",
             };
-            var wPattern = @"^(?=.*[A-Za-z])(.)(.)(.)\2\1$";
+            var wPattern = @"^(?=.*[\p{IsHiragana}\p{IsKatakana}\p{IsCJKUnifiedIdeographs}A-Za-z])(.)(.)(.)\2\1$";
             foreach (var wString in wStrings) {
                 var wMatches = Regex.Matches(wString, wPattern);
-                if (wMatches.Count > 0) {
-                    Console.WriteLine($"{wString}は5文字の回文です。");
-                } else {
-                    Console.WriteLine($"{wString}は5文字の回文ではありません。");
-                }
+                if (wMatches.Count > 0) Console.WriteLine($"{wString}は5文字の回文です。");
+                else Console.WriteLine($"{wString}は5文字の回文ではありません。");
             }
             Console.WriteLine($"{Environment.NewLine}終了するには何かキーを押してください...");
             Console.ReadKey();
