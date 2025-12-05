d694deb5280860fe4bbcf75de24ec20bd502baeb
ohashi <ohashi@beingcorp.co.jp>
2025-12-05 14:12:17 +0900

    #581846[C#テキスト]13章演習問題
    問題文を追加しました

--- 章末問題13/Practice13-1/Program13-1.cs
+++ 章末問題13/Practice13-1/Program13-1.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/* 問題13.1
+本文で利用したデータベースを利用し、以下のコードを書いてください。
+1.以下の2名の著者と4冊の書籍を追加してください。
 
+2.すべての書籍情報を著者名とともに表示するコードを書き、上記1.のデータが正しく追加されたか確認してください。
+
+3.タイトルの最も長い書籍を求めてください。複数ある場合は、すべてを求めてください。
+
+4.発行年の古い順に3冊だけ書籍を取得し、そのタイトルと著者名を求めてください。
+
+5.著者事に書籍のタイトルと発行年を表示してください。なお、著者は誕生日の遅い順に並べてください。
+*/
 namespace Practice13_1 {
     internal class Program {
         static void Main(string[] args) {
