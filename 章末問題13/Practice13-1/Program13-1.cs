2eb60ab6b2d1c24cc6e90733ba8588833621dda8
ohashi <ohashi@beingcorp.co.jp>
2025-12-08 09:45:06 +0900

    #581846[C#テキスト]13章演習問題
    練習問題を解答しました（一時保存用です）

--- 章末問題13/Practice13-1/Program13-1.cs
+++ 章末問題13/Practice13-1/Program13-1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Practice13_1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,35 @@ using System.Threading.Tasks;
 namespace Practice13_1 {
     internal class Program {
         static void Main(string[] args) {
+            InsertBooks();
         }
+        static void InsertBooks() {
+            using (var db = new BooksDbContext()) {
+                var book1 = new Book {
+                    Title = "坊ちゃん",
+                    PublishedYear = 2003,
+                    Author = new Author {
+                        Birthday = new DateTime(1867, 2, 9),
+                        Gender = "M",
+                        Name = "夏目漱石",
+                    }
+                };
+                db.Books.Add(book1);
+                var book2 = new Book {
+                    Title = "人間失格",
+                    PublishedYear = 1990,
+                    Author = new Author {
+                        Birthday = new DateTime(1909, 6, 19),
+                        Gender = "M",
+                        Name = "太宰治",
+                    }
+                };
+                db.Books.Add(book2);
+                db.SaveChanges();
+                Console.WriteLine($"{book1.Id},{book2.Id}");
+            }
+        }
+
+
     }
 }
